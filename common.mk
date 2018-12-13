fb609ddc37d21c9d92eb6739ae0a609e651755b2
Christos Papageorgiou (root-expert) <root.expert.xda@gmail.com>
2018-12-13 23:44:43 +0100

    sdm845-common: Add init script for JamesDSP.
    * Remount vendor as rw.
    * touch libjamesdsp lib in /vendor.
    * Remount vendor as ro.
    * This will allow us to bind mount libjamesdsp.so lib from /system
    
    Change-Id: Ib2a0f37f638da8f0014610b788b5175a6c355f11

--- common.mk
+++ common.mk
@@ -64,7 +64,8 @@ PRODUCT_PACKAGES_DEBUG += \
 # Common init scripts
 PRODUCT_PACKAGES += \
     init.qcom.rc \
-    init.recovery.qcom.rc
+    init.recovery.qcom.rc \
+    init.jamesdsp.sh
 
 # Display
 PRODUCT_PACKAGES += \
