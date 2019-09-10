43ff0d64389688068bcb3b4aeb35f605a7dd7dc5
doc HD <doc.divxm@gmail.com>
2019-09-10 21:13:05 +0300

    sdm845-common: don't build telephony-ext
    Change-Id: I6a5204567c73c0fe63cd7ef0189ab47e4ece45c9

--- common.mk
+++ common.mk
@@ -133,11 +133,11 @@ PRODUCT_PACKAGES += \
     RemovePackages
 
 # Telephony
-PRODUCT_PACKAGES += \
-    telephony-ext
+#PRODUCT_PACKAGES += \
+#    telephony-ext
 
-PRODUCT_BOOT_JARS += \
-    telephony-ext
+#PRODUCT_BOOT_JARS += \
+#    telephony-ext
 
 # Touch
 PRODUCT_PACKAGES += \
