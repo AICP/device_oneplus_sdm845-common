Myself5/android_device_oneplus_sdm845-common

sdm845: Make KeyHandler configurable

KeyHandler/src/org/carbonrom/settings/slider_settings/ButtonSettingsActivity.java
@@ -1,0 +1,31 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ *           (C) 2017 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.carbonrom.settings.slider_settings;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class ButtonSettingsActivity extends PreferenceActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getFragmentManager().beginTransaction().replace(android.R.id.content,
+                new ButtonSettingsFragment()).commit();
+    }
+}

KeyHandler/src/org/carbonrom/settings/slider_settings/ButtonSettingsFragment.java
@@ -1,0 +1,72 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ *               2017 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.carbonrom.settings.slider_settings;
+
+import android.os.Bundle;
+import android.support.v14.preference.PreferenceFragment;
+import android.support.v14.preference.SwitchPreference;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v7.preference.PreferenceGroup;
+import android.text.TextUtils;
+import android.view.MenuItem;
+
+import com.android.internal.util.cr.FileUtils;
+import org.carbonrom.settings.slider_settings.utils.Constants;
+
+public class ButtonSettingsFragment extends PreferenceFragment
+        implements OnPreferenceChangeListener {
+
+    private ListPreference mTopKeyPref;
+    private ListPreference mMiddleKeyPref;
+    private ListPreference mBottomKeyPref;
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.button_panel);
+        getActivity().getActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mTopKeyPref = (ListPreference) findPreference(Constants.NOTIF_SLIDER_TOP_KEY);
+        mTopKeyPref.setValueIndex(Constants.getPreferenceInt(getContext(), Constants.NOTIF_SLIDER_TOP_KEY));
+        mTopKeyPref.setOnPreferenceChangeListener(this);
+        mMiddleKeyPref = (ListPreference) findPreference(Constants.NOTIF_SLIDER_MIDDLE_KEY);
+        mMiddleKeyPref.setValueIndex(Constants.getPreferenceInt(getContext(), Constants.NOTIF_SLIDER_MIDDLE_KEY));
+        mMiddleKeyPref.setOnPreferenceChangeListener(this);
+        mBottomKeyPref = (ListPreference) findPreference(Constants.NOTIF_SLIDER_BOTTOM_KEY);
+        mBottomKeyPref.setValueIndex(Constants.getPreferenceInt(getContext(), Constants.NOTIF_SLIDER_BOTTOM_KEY));
+        mBottomKeyPref.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        Constants.setPreferenceInt(getContext(), preference.getKey(), Integer.parseInt((String) newValue));
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        // Respond to the action bar's Up/Home button
+        case android.R.id.home:
+            getActivity().finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}

KeyHandler/src/org/carbonrom/settings/slider_settings/ConfigPanelSearchIndexablesProvider.java
@@ -1,0 +1,84 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ * Copyright (C) 2017 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.carbonrom.settings.slider_settings;
+
+
+import android.database.Cursor;
+import android.database.MatrixCursor;
+import android.provider.SearchIndexableResource;
+import android.provider.SearchIndexablesProvider;
+
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_CLASS_NAME;
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_ICON_RESID;
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_INTENT_ACTION;
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_INTENT_TARGET_CLASS;
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_INTENT_TARGET_PACKAGE;
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_RANK;
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_RESID;
+import static android.provider.SearchIndexablesContract.INDEXABLES_RAW_COLUMNS;
+import static android.provider.SearchIndexablesContract.INDEXABLES_XML_RES_COLUMNS;
+import static android.provider.SearchIndexablesContract.NON_INDEXABLES_KEYS_COLUMNS;
+
+public class ConfigPanelSearchIndexablesProvider extends SearchIndexablesProvider {
+    private static final String TAG = "ConfigPanelSearchIndexablesProvider";
+
+    public static final int SEARCH_IDX_BUTTON_PANEL = 0;
+    public static final int SEARCH_IDX_GESTURE_PANEL = 1;
+    public static final int SEARCH_IDX_OCLICK_PANEL = 2;
+
+    private static SearchIndexableResource[] INDEXABLE_RES = new SearchIndexableResource[]{
+            new SearchIndexableResource(1, R.xml.button_panel,
+                    ButtonSettingsActivity.class.getName(),
+                    R.drawable.ic_settings_additional_buttons),
+    };
+
+    @Override
+    public boolean onCreate() {
+        return true;
+    }
+
+    @Override
+    public Cursor queryXmlResources(String[] projection) {
+        MatrixCursor cursor = new MatrixCursor(INDEXABLES_XML_RES_COLUMNS);
+        return cursor;
+    }
+
+    private static Object[] generateResourceRef(SearchIndexableResource sir) {
+        Object[] ref = new Object[7];
+        ref[COLUMN_INDEX_XML_RES_RANK] = sir.rank;
+        ref[COLUMN_INDEX_XML_RES_RESID] = sir.xmlResId;
+        ref[COLUMN_INDEX_XML_RES_CLASS_NAME] = null;
+        ref[COLUMN_INDEX_XML_RES_ICON_RESID] = sir.iconResId;
+        ref[COLUMN_INDEX_XML_RES_INTENT_ACTION] = "com.android.settings.action.EXTRA_SETTINGS";
+        ref[COLUMN_INDEX_XML_RES_INTENT_TARGET_PACKAGE] = "org.carbonrom.settings.slider_settings";
+        ref[COLUMN_INDEX_XML_RES_INTENT_TARGET_CLASS] = sir.className;
+        return ref;
+    }
+
+    @Override
+    public Cursor queryRawData(String[] projection) {
+        MatrixCursor cursor = new MatrixCursor(INDEXABLES_RAW_COLUMNS);
+        return cursor;
+    }
+
+    @Override
+    public Cursor queryNonIndexableKeys(String[] projection) {
+        MatrixCursor cursor = new MatrixCursor(NON_INDEXABLES_KEYS_COLUMNS);
+        return cursor;
+    }
+}

KeyHandler/src/org/carbonrom/settings/slider_settings/utils/Constants.java
@@ -1,0 +1,78 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ * Copyright (C) 2017 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.carbonrom.settings.slider_settings.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.support.v14.preference.SwitchPreference;
+import android.support.v7.preference.PreferenceManager;
+
+import android.media.AudioManager;
+
+public class Constants {
+
+    // Preference keys
+    public static final String NOTIF_SLIDER_TOP_KEY = "keycode_top_position";
+    public static final String NOTIF_SLIDER_MIDDLE_KEY = "keycode_middle_position";
+    public static final String NOTIF_SLIDER_BOTTOM_KEY = "keycode_bottom_position";
+
+    // Button prefs
+    public static final String NOTIF_SLIDER_TOP_PREF = "pref_keycode_top_position";
+    public static final String NOTIF_SLIDER_MIDDLE_PREF = "pref_keycode_middle_position";
+    public static final String NOTIF_SLIDER_BOTTOM_PREF = "pref_keycode_bottom_position";
+
+    // Default values
+    public static final int KEY_VALUE_TOTAL_SILENCE = 0;
+    public static final int KEY_VALUE_ALARMS_ONLY = 1;
+    public static final int KEY_VALUE_PRIORTY_ONLY = 2;
+    public static final int KEY_VALUE_VIBRATE = 3;
+    public static final int KEY_VALUE_NORMAL = 4;
+
+    public static final Map<String, String> sStringKeyPreferenceMap = new HashMap<>();
+    public static final Map<Integer, String> sKeyMap = new HashMap<>();
+    public static final Map<String, Integer> sKeyDefaultMap = new HashMap<>();
+
+    static {
+        sStringKeyPreferenceMap.put(NOTIF_SLIDER_TOP_KEY, NOTIF_SLIDER_TOP_PREF);
+        sStringKeyPreferenceMap.put(NOTIF_SLIDER_MIDDLE_KEY, NOTIF_SLIDER_MIDDLE_PREF);
+        sStringKeyPreferenceMap.put(NOTIF_SLIDER_BOTTOM_KEY, NOTIF_SLIDER_BOTTOM_PREF);
+
+        sKeyMap.put(603, NOTIF_SLIDER_TOP_KEY);
+        sKeyMap.put(602, NOTIF_SLIDER_MIDDLE_KEY);
+        sKeyMap.put(601, NOTIF_SLIDER_BOTTOM_KEY);
+
+        sKeyDefaultMap.put(NOTIF_SLIDER_TOP_KEY, KEY_VALUE_TOTAL_SILENCE);
+        sKeyDefaultMap.put(NOTIF_SLIDER_MIDDLE_KEY, KEY_VALUE_VIBRATE);
+        sKeyDefaultMap.put(NOTIF_SLIDER_BOTTOM_KEY, KEY_VALUE_NORMAL);
+    }
+
+    public static int getPreferenceInt(Context context, String key) {
+        return Settings.System.getIntForUser(context.getContentResolver(),
+                sStringKeyPreferenceMap.get(key), sKeyDefaultMap.get(key), UserHandle.USER_CURRENT);
+    }
+
+    public static void setPreferenceInt(Context context, String key, int value) {
+        Settings.System.putIntForUser(context.getContentResolver(),
+                sStringKeyPreferenceMap.get(key), value, UserHandle.USER_CURRENT);
+    }
+}

AICP/device_oneplus_sdm845-common

Change-Id: I5c1182fa775a64da6e0dee1848461ea9ca84e91d

KeyHandler/src/org/lineageos/settings/slider_settings/ButtonSettingsActivity.java
@@ -1,0 +1,31 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ *           (C) 2017 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings.slider_settings;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class ButtonSettingsActivity extends PreferenceActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getFragmentManager().beginTransaction().replace(android.R.id.content,
+                new ButtonSettingsFragment()).commit();
+    }
+}

KeyHandler/src/org/lineageos/settings/slider_settings/ButtonSettingsFragment.java
@@ -1,0 +1,72 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ *               2017 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings.slider_settings;
+
+import android.os.Bundle;
+import android.support.v14.preference.PreferenceFragment;
+import android.support.v14.preference.SwitchPreference;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v7.preference.PreferenceGroup;
+import android.text.TextUtils;
+import android.view.MenuItem;
+
+import com.android.internal.util.aicp.FileUtils;
+import org.lineageos.settings.slider_settings.utils.Constants;
+
+public class ButtonSettingsFragment extends PreferenceFragment
+        implements OnPreferenceChangeListener {
+
+    private ListPreference mTopKeyPref;
+    private ListPreference mMiddleKeyPref;
+    private ListPreference mBottomKeyPref;
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.button_panel);
+        getActivity().getActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mTopKeyPref = (ListPreference) findPreference(Constants.NOTIF_SLIDER_TOP_KEY);
+        mTopKeyPref.setValueIndex(Constants.getPreferenceInt(getContext(), Constants.NOTIF_SLIDER_TOP_KEY));
+        mTopKeyPref.setOnPreferenceChangeListener(this);
+        mMiddleKeyPref = (ListPreference) findPreference(Constants.NOTIF_SLIDER_MIDDLE_KEY);
+        mMiddleKeyPref.setValueIndex(Constants.getPreferenceInt(getContext(), Constants.NOTIF_SLIDER_MIDDLE_KEY));
+        mMiddleKeyPref.setOnPreferenceChangeListener(this);
+        mBottomKeyPref = (ListPreference) findPreference(Constants.NOTIF_SLIDER_BOTTOM_KEY);
+        mBottomKeyPref.setValueIndex(Constants.getPreferenceInt(getContext(), Constants.NOTIF_SLIDER_BOTTOM_KEY));
+        mBottomKeyPref.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        Constants.setPreferenceInt(getContext(), preference.getKey(), Integer.parseInt((String) newValue));
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        // Respond to the action bar's Up/Home button
+        case android.R.id.home:
+            getActivity().finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}

KeyHandler/src/org/lineageos/settings/slider_settings/ConfigPanelSearchIndexablesProvider.java
@@ -1,0 +1,84 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ * Copyright (C) 2017 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings.slider_settings;
+
+
+import android.database.Cursor;
+import android.database.MatrixCursor;
+import android.provider.SearchIndexableResource;
+import android.provider.SearchIndexablesProvider;
+
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_CLASS_NAME;
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_ICON_RESID;
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_INTENT_ACTION;
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_INTENT_TARGET_CLASS;
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_INTENT_TARGET_PACKAGE;
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_RANK;
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_RESID;
+import static android.provider.SearchIndexablesContract.INDEXABLES_RAW_COLUMNS;
+import static android.provider.SearchIndexablesContract.INDEXABLES_XML_RES_COLUMNS;
+import static android.provider.SearchIndexablesContract.NON_INDEXABLES_KEYS_COLUMNS;
+
+public class ConfigPanelSearchIndexablesProvider extends SearchIndexablesProvider {
+    private static final String TAG = "ConfigPanelSearchIndexablesProvider";
+
+    public static final int SEARCH_IDX_BUTTON_PANEL = 0;
+    public static final int SEARCH_IDX_GESTURE_PANEL = 1;
+    public static final int SEARCH_IDX_OCLICK_PANEL = 2;
+
+    private static SearchIndexableResource[] INDEXABLE_RES = new SearchIndexableResource[]{
+            new SearchIndexableResource(1, R.xml.button_panel,
+                    ButtonSettingsActivity.class.getName(),
+                    R.drawable.ic_settings_additional_buttons),
+    };
+
+    @Override
+    public boolean onCreate() {
+        return true;
+    }
+
+    @Override
+    public Cursor queryXmlResources(String[] projection) {
+        MatrixCursor cursor = new MatrixCursor(INDEXABLES_XML_RES_COLUMNS);
+        return cursor;
+    }
+
+    private static Object[] generateResourceRef(SearchIndexableResource sir) {
+        Object[] ref = new Object[7];
+        ref[COLUMN_INDEX_XML_RES_RANK] = sir.rank;
+        ref[COLUMN_INDEX_XML_RES_RESID] = sir.xmlResId;
+        ref[COLUMN_INDEX_XML_RES_CLASS_NAME] = null;
+        ref[COLUMN_INDEX_XML_RES_ICON_RESID] = sir.iconResId;
+        ref[COLUMN_INDEX_XML_RES_INTENT_ACTION] = "com.android.settings.action.EXTRA_SETTINGS";
+        ref[COLUMN_INDEX_XML_RES_INTENT_TARGET_PACKAGE] = "org.lineageos.settings.slider_settings";
+        ref[COLUMN_INDEX_XML_RES_INTENT_TARGET_CLASS] = sir.className;
+        return ref;
+    }
+
+    @Override
+    public Cursor queryRawData(String[] projection) {
+        MatrixCursor cursor = new MatrixCursor(INDEXABLES_RAW_COLUMNS);
+        return cursor;
+    }
+
+    @Override
+    public Cursor queryNonIndexableKeys(String[] projection) {
+        MatrixCursor cursor = new MatrixCursor(NON_INDEXABLES_KEYS_COLUMNS);
+        return cursor;
+    }
+}

KeyHandler/src/org/lineageos/settings/slider_settings/utils/Constants.java
@@ -1,0 +1,78 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ * Copyright (C) 2017 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings.slider_settings.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.support.v14.preference.SwitchPreference;
+import android.support.v7.preference.PreferenceManager;
+
+import android.media.AudioManager;
+
+public class Constants {
+
+    // Preference keys
+    public static final String NOTIF_SLIDER_TOP_KEY = "keycode_top_position";
+    public static final String NOTIF_SLIDER_MIDDLE_KEY = "keycode_middle_position";
+    public static final String NOTIF_SLIDER_BOTTOM_KEY = "keycode_bottom_position";
+
+    // Button prefs
+    public static final String NOTIF_SLIDER_TOP_PREF = "pref_keycode_top_position";
+    public static final String NOTIF_SLIDER_MIDDLE_PREF = "pref_keycode_middle_position";
+    public static final String NOTIF_SLIDER_BOTTOM_PREF = "pref_keycode_bottom_position";
+
+    // Default values
+    public static final int KEY_VALUE_TOTAL_SILENCE = 0;
+    public static final int KEY_VALUE_ALARMS_ONLY = 1;
+    public static final int KEY_VALUE_PRIORTY_ONLY = 2;
+    public static final int KEY_VALUE_VIBRATE = 3;
+    public static final int KEY_VALUE_NORMAL = 4;
+
+    public static final Map<String, String> sStringKeyPreferenceMap = new HashMap<>();
+    public static final Map<Integer, String> sKeyMap = new HashMap<>();
+    public static final Map<String, Integer> sKeyDefaultMap = new HashMap<>();
+
+    static {
+        sStringKeyPreferenceMap.put(NOTIF_SLIDER_TOP_KEY, NOTIF_SLIDER_TOP_PREF);
+        sStringKeyPreferenceMap.put(NOTIF_SLIDER_MIDDLE_KEY, NOTIF_SLIDER_MIDDLE_PREF);
+        sStringKeyPreferenceMap.put(NOTIF_SLIDER_BOTTOM_KEY, NOTIF_SLIDER_BOTTOM_PREF);
+
+        sKeyMap.put(603, NOTIF_SLIDER_TOP_KEY);
+        sKeyMap.put(602, NOTIF_SLIDER_MIDDLE_KEY);
+        sKeyMap.put(601, NOTIF_SLIDER_BOTTOM_KEY);
+
+        sKeyDefaultMap.put(NOTIF_SLIDER_TOP_KEY, KEY_VALUE_TOTAL_SILENCE);
+        sKeyDefaultMap.put(NOTIF_SLIDER_MIDDLE_KEY, KEY_VALUE_VIBRATE);
+        sKeyDefaultMap.put(NOTIF_SLIDER_BOTTOM_KEY, KEY_VALUE_NORMAL);
+    }
+
+    public static int getPreferenceInt(Context context, String key) {
+        return Settings.System.getIntForUser(context.getContentResolver(),
+                sStringKeyPreferenceMap.get(key), sKeyDefaultMap.get(key), UserHandle.USER_CURRENT);
+    }
+
+    public static void setPreferenceInt(Context context, String key, int value) {
+        Settings.System.putIntForUser(context.getContentResolver(),
+                sStringKeyPreferenceMap.get(key), value, UserHandle.USER_CURRENT);
+    }
+}