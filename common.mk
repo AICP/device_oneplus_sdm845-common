09ae75af654bdf7166be610f0b83a0204be7dddb
LuK1337 <priv.luk@gmail.com>
2021-08-29 19:18:43 +0200

    sdm845-common: Bring in vndk-30 support ^.^
    It'd be *really* nice to properly support both Q and R vendor images but
    unfortunately due to my unwillingness to put more than necessary effort
    into doing so the following things will not work anymore on Q vendor:
      - DASH
      - ANT+
    
    Again, ideally, we'd support them but the real point here is to have Q
    barely supported so that *hopefully*, people will migrate to R instead
    of staying on legacy firmware.
    
    Change-Id: Ibe80d6f12c039f68d3240d4a5f23752f9a146ca3

--- common.mk
+++ common.mk
@@ -45,8 +45,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
 
 # VNDK
-PRODUCT_TARGET_VNDK_VERSION := 29
-PRODUCT_EXTRA_VNDK_VERSIONS := $(PRODUCT_TARGET_VNDK_VERSION)
+PRODUCT_TARGET_VNDK_VERSION := 30
+PRODUCT_EXTRA_VNDK_VERSIONS := 29
 
 # Permissions
 PRODUCT_COPY_FILES += \
@@ -73,7 +73,7 @@ PRODUCT_PACKAGES += \
 
 # ANT+
 PRODUCT_PACKAGES += \
-    AntHalService
+    AntHalService-Soong
 
 # Audio
 PRODUCT_PACKAGES += \
@@ -105,11 +105,12 @@ PRODUCT_PACKAGES += \
 
 # Display
 PRODUCT_PACKAGES += \
-    libdisplayconfig \
+    libdisplayconfig.qti \
     libqdMetaData \
     libqdMetaData.system \
     libvulkan \
-    vendor.display.config@1.0
+    vendor.display.config@1.0 \
+    vendor.display.config@2.0
 
 # DPM
 PRODUCT_PACKAGES += \
@@ -126,7 +127,7 @@ PRODUCT_PACKAGES += \
 
 # HotwordEnrollement app permissions
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-hotword.xml
+    $(LOCAL_PATH)/configs/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
 
 # Input
 PRODUCT_COPY_FILES += \
@@ -140,6 +141,9 @@ PRODUCT_PACKAGES += \
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_profiles_vendor.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/media_profiles_vendor.xml
+
+PRODUCT_PACKAGES += \
+    libavservices_minijail
 
 # Net
 PRODUCT_PACKAGES += \
